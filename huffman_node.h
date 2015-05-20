3d9f047c9c095f9eb3606701ee0a3a34c83f1cb4
MarceloAbio <marceloabio@yahoo.com.br>
2015-05-20 11:00:59 -0300

    documentação.

--- huffman_node.h
+++ huffman_node.h
@@ -20,12 +20,31 @@ struct huffmanNode {
 	struct huffmanNode *right;
 };
 
+/*
+*Cria o nó de huffman vazio.
+*@Retona o ponteiro do nó emptyHuffmanNode.
+*/
 HuffmanNode* HuffmanNode_createEmpty();
 
+/*
+*Cria o nó de huffman com os dados recebidos.
+*@Recebe o caracter character, o inteiro frequency, o ponteiro para o nó left e o ponteiro para o nó right.
+*@Retorna o ponteiro para o nó huffmanNode.
+*/
 HuffmanNode* HuffmanNode_createNode(char character, int frequency, HuffmanNode *left, HuffmanNode *right);
 
+/*
+*Cria um nó que representa uma folha da arvore de Huffman com os valores dados.
+*@Recebe o caracter character e o inteiro frequency.
+*@Retorna o ponteiro para o nó leafHuffmanNode.
+*/
 HuffmanNode* HuffmanNode_createLeaf(char character, int frequency);
 
+/*
+*Verifica se o nó recebido é uma folha.
+*@Recebe o ponteiro ponteiro para o nó huffmanNode.
+*@Retorna TRUE se for uma folha e FALSE caso contrário.
+*/
 int HuffmanNode_isLeaf(HuffmanNode *huffmanNode);
 
 #endif /* HUFFMAN_NODE_H_ */
