6f646647fbf8aa54188800ca97f097347d0e7491
MarceloAbio <marceloabio@yahoo.com.br>
2015-05-20 10:45:23 -0300

    documentação.
    Adicionei o parametro da fila de frequencia no contrato de  HuffmanTree_build, que estava ausente.

--- huffman_tree.h
+++ huffman_tree.h
@@ -23,16 +23,42 @@ struct huffmanTree {
 	char *table[MAX];
 };
 
+/*
+*Cria a arvore de Huffman vazia, com ponteiros apontando para VAZIO.
+*@Retorna ponteiro para a arvore emptyHuffmantree.
+*/
 HuffmanTree* HuffmanTree_createEmpty();
 
-HuffmanTree* HuffmanTree_build();
+/*
+*Gera a arvore de Huffman a partir da fila de frequencia.
+*@Recebe o ponteiro para a fila frequencyQueue.
+*@Retorna o ponteiro huffmanTree.
+*/
+HuffmanTree* HuffmanTree_build(FrequencyQueue *frequencyQueue);
 
+/*
+*Verifica se a arvore de Huffman está vazia.
+*@Recebe o ponteiro para a arvore huffmanTree.
+*@Retorna TRUE e estiver vazia e FALSE caso o contrario.
+*/
 int HuffmanTree_isEmpty(HuffmanTree *huffmanTree);
 
+/*
+*Gera a tabela de codigo da arvore de Huffman.
+*@Recebe o ponteiro para a arvore huffmanTree.
+*/
 void HuffmanTree_fillTable(HuffmanTree *huffmanTree);
 
+/*
+*Gera o codigo para cada caractere da arvore.
+*@Recebe o ponteiro para a arvore huffmanTree, o ponteiro para o nó root e o ponteiro para a lista linkedList.
+*/
 void HuffmanTree_catchCode(HuffmanTree *huffmanTree, HuffmanNode *root, LinkedList *linkedList);
 
+/*
+*Imprime a tabela de codigo da arvore.
+*@Recebe o ponteiro para a arvore huffmanTree.
+*/
 void HuffmanTree_printTable(HuffmanTree *huffmanTree);
 
 #endif /* HUFFMAN_TREE_H_ */
