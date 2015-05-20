931b7618244d7ba3dab944dcd005b49b07ef6abd
MarceloAbio <marceloabio@yahoo.com.br>
2015-05-20 10:11:55 -0300

    documentação.

--- linked_list.h
+++ linked_list.h
@@ -24,12 +24,28 @@ struct linkedList {
 	int length;
 };
 
+/*
+*Cria lista encadeada vazia, com ponteiro avontando para VAZIO e tamanho 0.
+*@Retorna ponteiro linkedList.
+*/
 LinkedList* LinkedList_createEmpty();
 
+/*
+*Insere um nó na lista encadeada.
+*@Recebe ponteiro linkedList e caractere bit.
+*/
 void LinkedList_insert(LinkedList *linkedList, char bit);
 
+/*
+*Deleta o primeiro nó da lista encadeada.
+*@Recebe ponteiro linkedList.
+*/
 void LinkedList_deleteFirst(LinkedList *linkedList);
 
+/*
+*Imprime a lista encadeada.
+*@Recebe ponteiro linkedList.
+*/
 void LinkedList_print(LinkedList *linkedList);
 
 #endif /* LINKED_LIST_H_ */
